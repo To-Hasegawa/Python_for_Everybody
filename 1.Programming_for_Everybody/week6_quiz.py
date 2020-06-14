1d13a39ab73af3784c6bcf867af553d135659123
To-Hasegawa <hasegawa16777216@gmail.com>
2020-06-14 12:56:48 +0900

    Create week6_quiz.py

---
+++ 1.Programming_for_Everybody/week6_quiz.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+def stuff():
+    print('Hello')
+    return
+    print('World')
+
+stuff()
+
+
+def addtwo(a, b):
+    added = a + b
+    return a
+
+x = addtwo(2, 7)
+print(x)
